3c7c1248a46d58f8d1531d8e141f3b4e93d0217c
eilonben <eilonben@post.bgu.ac.il>
2020-06-03 13:11:20 +0300

    added comments, deleted unused code sections

--- editor/js/ActionsBP.js
+++ editor/js/ActionsBP.js
@@ -8,7 +8,7 @@ ActionsBP.prototype.init = function (actions) {
     var graph = editor.graph;
     var mod = graph.getModel();
     var deb = new debuggerBP(ui);
-
+    // The action that connects between creating the console window and the button to open it
     function showConsole() {
         if (this.consoleWindow === null || this.consoleWindow === undefined) {
             this.consoleWindow = new myConsoleWindow(ui, document.body.offsetWidth - 480, 120, 420, 285);
@@ -30,6 +30,7 @@ ActionsBP.prototype.init = function (actions) {
     actions.addAction('showConsole', function () {
         showConsole.call(this);
     });
+    //The action that occurs while clicking the "Open Code Editor" on the general block
     actions.addAction('editCode', function () {
         var cell = graph.getSelectionCell() || graph.getModel().getRoot();
 
@@ -39,51 +40,50 @@ ActionsBP.prototype.init = function (actions) {
             dlg.init();
         }
     });
-
+    //The action that occurs while clicking the "Play" button in order to execute the BP program
     actions.addAction('runModel', function () {
         var cells = graph.getModel().cells;
 
         var code = mxUtils.getPrettyXml(ui.editor.getGraphXml());
-        console.log(code);
 
         // if invalidCells is not empty -> there are edges without source or target OR start node without edges
         var invalidCells = findInvalidCells(graph.getModel());
 
         //Confirm that the graph is valid
-        if(invalidCells.length != 0)
+        if(invalidCells.length !== 0)
             mxUtils.alert("Graph is Invalid! lonely start node or edge");
         else
         {
             showConsole.call(this);
             parse_graph(code, null);
-            mxUtils.alert("Code deployed");
         }
 
 
     }, null, null, 'Alt+Shift+R');
 
-    actions.addAction('editBsync', function () {
-        var cell = graph.getSelectionCell() || graph.getModel().getRoot();
+    // actions.addAction('editBsync', function () {
+    //     var cell = graph.getSelectionCell() || graph.getModel().getRoot();
+    //
+    //     if (cell != null) {
+    //         var dlg = new BSyncForm(ui, cell);
+    //         ui.showDialog(dlg.container, 520, 420, true, true);
+    //         dlg.init();
+    //     }
+    // });
 
-        if (cell != null) {
-            var dlg = new BSyncForm(ui, cell);
-            ui.showDialog(dlg.container, 520, 420, true, true);
-            dlg.init();
-        }
-    });
-
+    //The action that occurs when clicking on the "next step" button while in debug mode
     actions.addAction('debug_next', function() {
 
         deb.next();
 
     }, false, null);
-
+    //The action that occurs when clicking on the "previous step" button while in debug mode
     actions.addAction('debug_back', function() {
 
         deb.back();
 
     }, false, null);
-
+    //The action that occurs when clicking on the "stop" button while in debug mode
     actions.addAction('debug_stop', function() {
 
         deb.endDebugging();
